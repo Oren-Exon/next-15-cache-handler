03a82ec0096ffda7da25134c21f985b8fb353b3c
Oren Levi <orenl@exon-orenl.com>
2025-09-30 17:19:23 +0300

    implement cache handler

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig =  {
   /* config options here */
+  output: "standalone",
+  cacheHandler: "./cache-handler.mjs",
+  cacheMaxMemorySize: 0, // disable default in-memory caching
 };
 
 export default nextConfig;
