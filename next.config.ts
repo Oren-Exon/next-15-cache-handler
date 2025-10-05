aab7501d01b0da0614b037775d27c744c6824d82
Oren Levi <orenl@exon-orenl.com>
2025-10-05 17:05:00 +0300

    fix `dev` script

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig =  {
-  /* config options here */
-  output: "standalone",
-  cacheHandler: "./cache-handler.mjs",
-  cacheMaxMemorySize: 0, // disable default in-memory caching
-};
+const nextConfig: NextConfig = () =>
+  {
+    return  {
+      /* config options here */
+      output: "standalone",
+      cacheHandler: process.env.NODE_ENV === "production" ? "./cache-handler.mjs" : undefined,
+      cacheMaxMemorySize: 0, // disable default in-memory caching
+    };
+  } 
 
 export default nextConfig;
